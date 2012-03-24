0e37c8a089e9041ce1b64eeebf315308e7f1d51c
Marcelo <modin@modin-MS-N033.(none)>
2012-03-24 01:13:11 -0300

    primer escenario de hw4 todo verde en cucumber

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,9 @@
+Given /the following movies exist:$/ do |movies_table|
+  movies_table.hashes.each do |movie|
+  Movie.create!(movie)
+  end
+end
+
+Then /the director of "(.*)" should be "(.*)"$/ do |movie, director|
+  assert (page.body.to_s =~ /#{director}/)
+end
---
+++ spec/movies_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe MoviesController do
+  describe 'edit director' do
+    it 'should call the model method that performs the update of the movie' do
+    end
+  end
+end
